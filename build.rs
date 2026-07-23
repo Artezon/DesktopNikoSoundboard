1e61b4e975d1b39d912adf6518099411bf07cf22
Artezon <artezon2003@gmail.com>
2026-07-23 03:56:06 +0300

    Update metadata description

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ fn main() {
     {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("assets/icon.ico")
-            .set("ProductName", "Niko")
-            .set("FileDescription", "Niko OneShot")
+            .set("ProductName", "Niko OneShot")
+            .set("FileDescription", "OneShot characters soundboard :3")
             .set("CompanyName", "Artezon");
         res.compile().unwrap();
     }
